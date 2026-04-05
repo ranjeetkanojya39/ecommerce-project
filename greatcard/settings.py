9cb4a2b85867409a9de74723848b21e097125e68
ranjeetkanojiya39 <ranjeetskanojiya20@gmail.com>
2026-04-06 01:41:20 +0530

    static files ready

--- greatcard/settings.py
+++ greatcard/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -56,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'greatcard.urls'
 
@@ -128,6 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT  = BASE_DIR   /'/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
@@ -153,3 +157,5 @@ EMAIL_HOST_PASSWORD = 'iyqq dxwu ovdj earb'
 EMAIL_USE_TLS = True
 
 DEFAULT_FROM_EMAIL = 'ranjeetskanojiya20@gmail.com'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
