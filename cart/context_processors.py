757a41d395b6173bc98fbd0d0bc76923e60c7f38
ranjeetkanojiya39 <ranjeetskanojiya20@gmail.com>
2026-04-06 20:59:35 +0530

    Fixed cart count bug and improved responsive UI for cart and product detail

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,5 +1,5 @@
 from .models import Cart, CartItem
-from  .  views import _cart_id
+from .views import _cart_id
 
 def counter(request):
     cart_count = 0
@@ -7,12 +7,22 @@ def counter(request):
         return {}
     else:
         try:
+            # 1. Cart fetch karein session id se
             cart = Cart.objects.filter(cart_id=_cart_id(request))
-            cart_item = CartItem.objects.all().filter(cart=cart[:1])
-            for cart_item in cart_item:
-                cart_count += cart_item.quantity
+            
+            # 2. LOGIC BUG FIX: Check karein agar user login hai
+            if request.user.is_authenticated:
+                # Login user ke saare cart items lo (Database user field se)
+                cart_items = CartItem.objects.all().filter(user=request.user)
+            else:
+                # Guest user ke liye session cart se items lo
+                cart_items = CartItem.objects.all().filter(cart=cart[:1])
+            
+            # 3. Loop karke total quantity calculate karein
+            for item in cart_items:
+                cart_count += item.quantity
+                
         except Cart.DoesNotExist:
-            cart_count  = 0
-    return dict(cart_count = cart_count)
-
-
+            cart_count = 0
+            
+    return dict(cart_count=cart_count)
