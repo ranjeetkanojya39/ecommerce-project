a9e34ae09bebee814d598a249bf6fbf08edc2a6f
ranjeetkanojiya39 <ranjeetskanojiya20@gmail.com>
2026-04-15 15:17:54 +0530

    update

--- accounts/views.py
+++ accounts/views.py
@@ -12,7 +12,7 @@ from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 
-from carts.views import _cart_id
+#from carts.views import _cart_id
 from cart.models import  Cart, CartItem
 
 
