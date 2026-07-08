4c631f95a59e2891b573c82e196c2e16593e9e44
ranjeetkanojiya39 <ranjeetskanojiya20@gmail.com>
2026-07-08 23:16:12 +0530

    add readme

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     
     path('category/<slug:category_slug>/', views.store, name='products_by_category'),
     path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
-    path('', views.store, name='store'),  # 👈 ye important hai
+    path('', views.store, name='store'),  
     path('search/', views.search, name='search'),
     path('place-order/', views.place_order, name='place_order'),
 ]
